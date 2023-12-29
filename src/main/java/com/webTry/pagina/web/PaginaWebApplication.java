810a800bffd14498c8eae50e349c9b725bcef349
thiagofmj <thiagofmj16@outlook.com>
2023-12-29 17:10:29 -0300

    Project created

---
+++ src/main/java/com/webTry/pagina/web/PaginaWebApplication.java
@@ -0,0 +1,13 @@
+package com.webTry.pagina.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginaWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webTry/pagina/web/PaginaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webTry.pagina.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
