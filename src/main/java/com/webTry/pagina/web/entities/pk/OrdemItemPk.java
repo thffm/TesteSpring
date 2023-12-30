9f1caf466599e9c3ddc7f4063803e88a3053142e
thiagofmj <thiagofmj16@outlook.com>
2023-12-30 19:07:23 -0300

    OrderItem, many-to-many association with extra attributes PARTE1

---
+++ src/main/java/com/webTry/pagina/web/entities/pk/OrdemItemPk.java
@@ -0,0 +1,50 @@
+package com.webTry.pagina.web.entities.pk;
+
+import com.webTry.pagina.web.entities.Order;
+import com.webTry.pagina.web.entities.Product;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+public class OrdemItemPk implements Serializable {
+    public static final Long serialVersionUID = 1L;
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrdemItemPk that = (OrdemItemPk) o;
+        return Objects.equals(order, that.order) && Objects.equals(product, that.product);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(order, product);
+    }
+}
